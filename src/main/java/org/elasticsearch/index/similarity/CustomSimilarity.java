9c99f8b70e19c27b215182d4a8f7cfb4bbbdc449
wangrl <ruilingwang@163.com>
2015-06-16 17:07:15 +0800

    初始化

---
+++ src/main/java/org/elasticsearch/index/similarity/CustomSimilarity.java
@@ -0,0 +1,11 @@
+package org.elasticsearch.index.similarity;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: wangrl
+ * @Date: 2015-06-16 17:04
+ */
+public class CustomSimilarity {
+
+}
---
+++ src/main/java/org/elasticsearch/plugin/similarity/customer/CustomerSimilarityPlugin.java
@@ -0,0 +1,11 @@
+package org.elasticsearch.plugin.similarity.customer;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: wangrl
+ * @Date: 2015-06-16 17:05
+ */
+public class CustomerSimilarityPlugin {
+
+}
